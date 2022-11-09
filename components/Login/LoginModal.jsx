40c5e275c3da1179d935d1006751b9232a023a08
TekinaTawar <astroaniket1@gmail.com>
2022-11-10 04:09:46 +0530

    refactored login to isolate modal

---
+++ components/Login/LoginModal.jsx
@@ -0,0 +1,146 @@
+import Modal from "components/shared/Modal";
+import Image from "next/legacy/image";
+import styled from "styled-components";
+
+import buttonBorder from "public/images/buttonBorder.svg";
+
+const _LoginModal = styled(Modal)`
+  //to position the content inside the modal
+  display: grid;
+  grid-auto-rows: repeat(4, 1fr);
+  justify-content: center;
+  align-content: space-around;
+  align-items: center;
+  justify-items: center;
+
+  > strong {
+    font-size: var(--step-1);
+    color: var(--golden-yellow3);
+  }
+
+  > .socialLogin {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    row-gap: 2rem; //spacing
+
+    & > h5 {
+      font-family: metropolis;
+      font-weight: 500;
+    }
+
+    > .socialIcons {
+      display: flex;
+      /* justify-content: space-around; */
+      /* background-color: pink; */
+      width: 100%;
+      justify-content: center;
+      column-gap: 1rem;
+      > * {
+        display: flex;
+        background: white;
+        border-radius: 50%;
+        width: 2rem;
+        height: 2rem;
+        color: black;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
+`;
+
+const WelcomeText = styled.h2`
+  font-family: stormfaze;
+  font-size: var(--step-0);
+  padding-bottom: 1rem;
+  border-bottom: 1px solid #ebac45;
+  display: inline-block;
+`;
+
+const PhNoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-self: stretch;
+  font-size: var(--step--2);
+  row-gap: 10px;
+
+  > .phNumberInput {
+    /* background-color: pink; */
+    border: 2px solid var(--golden-yellow1);
+    border-radius: 5px;
+    background-color: black;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    padding-left: 1rem;
+    font-size: var(--step--1);
+
+    :focus-within {
+      border: 2px solid white;
+    }
+
+    > input {
+      color: white;
+      /* border-radius: 5px; */
+      width: 100%;
+      margin-left: 1rem;
+      background-color: transparent;
+      /* width: auto; */
+      outline: none;
+      border: none;
+      height: 100%;
+      font-size: var(--step--1);
+      font-family: Metropolis;
+      border-left: 2px solid var(--golden-yellow1);
+      padding-inline: 1rem;
+      position: relative;
+
+      :focus-visible {
+        border-left: 2px solid white;
+      }
+    }
+  }
+`;
+
+const BorderButton = styled.button`
+  background-color: var(--military-green);
+  color: var(--golden-yellow4);
+  text-transform: uppercase;
+  font-family: metropolis;
+  font-weight: 800;
+  padding: 0.8rem 1.5rem; //maybe change later.
+  border-radius: 5px;
+  position: relative; //to position border;
+  border: none;
+`;
+
+const LoginModal = () => {
+  return (
+    <_LoginModal modalHead="Login">
+      <WelcomeText>WELCOME TO APTIWARROR</WelcomeText>
+
+      <PhNoSection>
+        <label htmlFor="phNumber">ENTER YOUR MOBILE NUMBER</label>
+        <group className="phNumberInput">
+          <span className="countryCode">+91</span>
+          <input type="tel" name="phNumber" id="phNumber" />
+        </group>
+      </PhNoSection>
+
+      <BorderButton>
+        <Image src={buttonBorder} alt="bg" layout="fill" objectFit="fill" />
+        CONTINUE
+      </BorderButton>
+      <strong>OR</strong>
+
+      <group className="socialLogin">
+        <h5>Login using your social media accounts</h5>
+        <group className="socialIcons">
+          <span>G</span> <span>F</span> <span>in</span>
+        </group>
+      </group>
+    </_LoginModal>
+  );
+};
+export default LoginModal;
---
+++ components/shared/Modal.jsx
@@ -0,0 +1,39 @@
+import Modalsvg from "public/images/Modal.svg";
+import Image from "next/legacy/image";
+
+import styled from "styled-components";
+
+const _Modal = styled.div`
+  position: relative; //to position the image
+  width: clamp(22.5rem, calc(6.51rem + 71.05vw), 30.94rem);
+  height: clamp(550px, 8vh, 650px);
+  padding-block: 4rem 2rem; // to accomodate the border top and bottom.
+  color: white;
+
+  & > * {
+    z-index: 2;
+  }
+
+  & > .modalHead {
+    position: absolute;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    font-family: "stormfaze";
+    font-size: var(--step-0);
+    color: #ffd976;
+    top: 1px;
+  }
+`;
+
+//the modal box with border that scales automatically.
+const Modal = ({ className, children, modalHead }) => {
+  return (
+    <_Modal className={className}>
+      <Image src={Modalsvg} alt="login modal" layout="fill" objectFit="fill" />
+      <h1 className="modalHead">{modalHead}</h1>
+      {children}
+    </_Modal>
+  );
+};
+export default Modal;
