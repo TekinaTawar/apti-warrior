7bde16a61880f090ffdf86edd47e9cb14c37c80d
TekinaTawar <astroaniket1@gmail.com>
2022-11-13 09:59:11 +0530

    refactored Diamond Button & added to dashboard

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import { FaMoneyBill } from "react-icons/fa";
-// import diamond icon from react-icons
 import { IoDiamondOutline } from "react-icons/io5";
 import { BsGear } from "react-icons/bs";
+
 import IconButton from "components/shared/IconButton";
 import book from "public/images/dashboard/book.svg";
 import analytics from "public/images/dashboard/analytics.svg";
 import trophy from "public/images/dashboard/trophy.svg";
+
 import IconButtonPrimary from "components/shared/IconButtonPrimary";
 import gun from "public/images/dashboard/gun.svg"
+
+import DiamondButton from "components/shared/DiamondButton";
+import dailyTask from "public/images/dashboard/dailyTask.svg";
+import socials from "public/images/dashboard/socials.svg";
+import shoppingCart from "public/images/dashboard/shoppingCart.svg";
 
 const MainContainer = styled.main`
   /* background-color: orange; */
@@ -130,6 +136,9 @@ const dashboard = () => {
           <BsGear />
         </i>
       </Header>
+      <DiamondButton icon={dailyTask} />
+      <DiamondButton icon={socials} />
+      <DiamondButton icon={shoppingCart} />
       <IconButton icon={book}>Training</IconButton>
       <IconButton icon={analytics}>Analytics</IconButton>
       <IconButton icon={trophy}>LeaderBoard</IconButton>
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,8 +1,8 @@
 //import useState from react
 
 import styled from "styled-components";
-import Image from "next/image";
-import dailyTask from "public/images/dashboard/dailyTask.svg";
+
+
 
 const MainContainer = styled.main`
   /* background-color: orange; */
@@ -15,51 +15,10 @@ const MainContainer = styled.main`
   align-items: center;
 `;
 
-const DiamondButton = styled.div`
-  width: var(--space-xl-2xl);
-  height: var(--space-xl-2xl);
-  /* background-color: red; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  & button {
-    width: 70.71%; // 100 / sqrt(2)
-    height: 70.71%;
-    transform: rotate(45deg);
-    border: var(--space-4xs-3xs) solid var(--primary-0);
-    background-color: var(--gray-200);
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    box-shadow: 0 0 var(--space-3xs-2xs) 1px var(--primary-0);
-
-    & .iconContainer {
-      width: 70.71%; // 100 / sqrt(2)
-      height: 70.71%;
-      /* background-color: orange; */
-      transform: rotate(-45deg);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      color: var(--primary-0);
-    }
-  }
-`;
-
 const test = () => {
   return (
     <MainContainer>
-      <DiamondButton>
-        <button>
-          {/* this is needed so that the image does not overflow out of the button */}
-          <div className="iconContainer">
-            <Image src={dailyTask} alt="daily task" fill/>
-          </div>
-        </button>
-      </DiamondButton>
+
     </MainContainer>
   );
 };
