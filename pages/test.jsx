43ca2fb4dce3ce0d134f8cbf98209f03f7df8056
TekinaTawar <astroaniket1@gmail.com>
2022-11-14 12:01:25 +0530

    refactored horizontal contianer out of the training

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,8 +1,8 @@
 //import useState from react
 
 import styled from "styled-components";
-
-
+import Image from "next/legacy/image";
+import HorizontalCardContainer from "components/shared/HorizontalCardContainer";
 
 const MainContainer = styled.main`
   /* background-color: orange; */
@@ -10,15 +10,18 @@ const MainContainer = styled.main`
   width: 100%;
   height: 100%;
   //center the content vertically and horizontally
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: fit-content(50px) 1fr;
+  grid-template-areas: "header header" "subjectsContainer modulesContainer";
+  column-gap: var(--space-xs-s);
+  row-gap: var(--space-2xs-xs);
 `;
 
 const test = () => {
   return (
     <MainContainer>
-
+      <HorizontalCardContainer />
     </MainContainer>
   );
 };
